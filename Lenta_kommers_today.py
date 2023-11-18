2fa10eb2b59cde6d8c35c644dd9be41ac57af682
evgeniy_MacBook <pavlinbl4@gmail.com>
2023-11-18 19:58:24 +0300

    refactoring

--- Lenta_kommers_today.py
+++ Lenta_kommers_today.py
@@ -1,50 +1,27 @@
 # pip install Pillow, pip install openpyxl, pip install fake-headers
-import os
-import requests
+
 import json
-from openpyxl.drawing.image import Image
 from datetime import datetime, timedelta
 
 from tools.create_subfolder import create_directory
+from tools.downloader import image_downloader
+from tools.get_file_name import file_name
 from tools.get_html import get_html
 from tools.lenta_ru_time import lenta_ru_time_converter
-
-yesterday = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
-report_folder = 'Users/Documents/Kommersant/LentaRU'
-
-
-def notification(message):
-    title = "Готово"
-    command = f'''
-    osascript -e 'display notification "{message}" with title "{title}"'
-    '''
-    os.system(command)
-
-
-def make_subfolder(photograf):
-    folder = f"{report_folder}/{yesterday}/{photograf}"
-    os.makedirs(folder, exist_ok=True)
-    return folder
-
-
-def download_image(folder, image_url, photograf, yesterday):
-    r = requests.get(image_url)
-    image_name = f"{yesterday}_{photograf}_{image_url.split('/')[-1]}"
-    with open(f"{folder}/{image_name}", 'wb') as download_file:
-        for chunk in r.iter_content(9000):
-            download_file.write(chunk)
-    return f"{folder}/{image_name}"
+from tools.system_notification import notification
 
 
 def main():
+    yesterday = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
+    folder = create_directory(f'Kommersant/LentaRU')
     number = 10
 
     while True:
         url = f"https://lenta.ru/search/v2/process?from=0&size={number}" \
               f"&sort=2&title_only=0&domain=1&modified,format=yyyy-MM-dd&query=фото Коммерсантъ"
-        rezult = json.loads(get_html(url))
+        result = json.loads(get_html(url))
         count = 0
-        for i in rezult['matches']:
+        for i in result['matches']:
             if lenta_ru_time_converter(i['pubdate']) == yesterday:  # today:
                 count += 1
                 photograf = i['text'].split('/ Коммерсантъ')[0][6:].strip()
@@ -52,19 +29,11 @@ def main():
                 # если под фото нед подписи и взята первая строка текста, то пропускаем снимок
                 if len(photograf) < 25:
                     image_url = i['image_url']
-                    # ws.row_dimensions[count].height = 100  # задаю высоту столбца
                     print(photograf, image_url, lenta_ru_time_converter(i['pubdate']))
 
-                    # folder = make_subfolder(photograf)
-                    folder = create_directory(f'Kommersant/LentaRU')
-                    image_path = download_image(folder, image_url, photograf, yesterday)
+                    image_name = f"{file_name(image_url)}_{photograf}.JPG"
 
-                    img = Image(image_path)
-                    resize_height = img.height // 3  # уменьшая рарешение в два раза
-                    resize_width = img.width // 3  # уменьшая рарешение в два раза
-
-                    img.width = resize_width  # устанавливаю размер превью
-                    img.height = resize_height  # устанавливаю размер превью
+                    image_downloader(image_url, folder, image_name)
 
         if count < 10:
             notification("LentaRu completed")
